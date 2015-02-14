12ff4b9e86b3b434624c7af47fbd1833a028e342
Kevin Isom <kevin.isom@gmail.com>
2015-02-14 17:16:46 +1300

    Get rss feeds in parallel

---
+++ retrieve/getRss.js
@@ -0,0 +1,19 @@
+var config = require('xtconf')();
+
+var feeds = config.get('feeds');
+
+var async = require('async');
+
+
+module.exports = function (mainCallback) {
+    var rss = require('parserss');
+
+    var requests = feeds.map(function (feed) {
+        return function(callback){
+            rss(feed, 1000, function (err, res) {
+                callback(err, res);
+            });
+        }
+    });
+    async.parallel(requests,mainCallback);
+};
