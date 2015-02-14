8c74e4fa02794bcb9b392449e611c3e286996dc0
Kevin Isom <kevin.isom@gmail.com>
2015-02-15 01:25:31 +1300

    Added routes for showing the data as well

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,17 @@ var rss = require('../retrieve/getRss');
 var story = require('../retrieve');
 var normalize = require('../utils/normalize');
 
+var safeEnclosure = function (article) {
+    var result = '';
+    if(article['rss:enclosure']) {
+        if (article['rss:enclosure']['@']) {
+            result = article['rss:enclosure']['@'].url;
+        }
+    }
+    return result;
 
-router.get('/', function(req, res, next) { 
+}
+router.get('/stories', function(req, res, next) { 
     rss(function (err, doc) { 
         var articles = doc.map(function (feed) {
             return feed.articles;
@@ -21,18 +30,37 @@ router.get('/', function(req, res, next) {
                 date: time,
                 pubDate: article.pubDate,
                 link: article.link,
-                image: article['rss:enclosure']['@'] ? article['rss:enclosure']['@'].url : ''
+                image: safeEnclosure(article)
             };
         });
         db.insert(mappedArticles);
-        res.send(mappedArticles.sort(function (a,b) {
+        mappedArticles.sort(function (a,b) {
             return a.pubDate > b.pubDate ? -1 : 1;
-        }));
+        });
+        res.render('index', {title: 'News Stand', stories: mappedArticles});
     });
 });
-
-
+router.get('/', function(req, res, next) { 
+    var db = require('../db')('stories');
+    db.find({}).limit(20).toArray(function(err, docs) {
+        res.render('index', {title: 'News Stand', stories: docs});
+    }); 
+});
 router.get('/story/:slug/:time', function(req, res, next) {
+    console.log('sluggo');
+    var db = require('../db')('stories');
+    var slug = req.params.slug + '/' + req.params.time;
+
+    db.findOne({slug:slug}, function (err, result) {
+        console.log('sluggo');
+        story(result.link, function (err, data) {
+            console.log('sluggo');
+            result.content = data.content;
+            res.render('story', { title: 'News Stand', story: result});
+        });
+    });
+});
+router.get('/storydata/:slug/:time', function(req, res, next) {
  
         var db = require('../db')('stories');
         var slug = req.params.slug + '/' + req.params.time;
@@ -40,8 +68,8 @@ router.get('/story/:slug/:time', function(req, res, next) {
         db.findOne({slug:slug}, function (err, result) {
 
             story(result.link, function (err, data) {
-                data.link = result.link;
-                res.send(data);
+                result.content = data.content;
+                res.send(result);
             });
         }); 
  
