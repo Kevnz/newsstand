1124508c2c90b1296c4c3ce872c0193f70499769
Kevin Isom <kevin.isom@gmail.com>
2015-02-14 23:16:27 +1300

    Use custom request so options can be passed to superagent

--- retrieve/index.js
+++ retrieve/index.js
@@ -1,14 +1,11 @@
 var config = require('xtconf')();
-
-var feeds = config.get('feeds');
-
-var async = require('async');
-
 var xray = require('x-ray');
+var request = require('x-ray/lib/request');
 
 module.exports = function (link, callback) {
+
     var selectors = config.get('extract'); 
-    
+
     var selector = selectors.filter(function (item) {
         console.log(link.indexOf(item.key));
         if (link.indexOf(item.key) > -1) {
@@ -17,6 +14,9 @@ module.exports = function (link, callback) {
     });
 
     xray(link)
+    .use(request( {
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36"  
+    }))
     .select(selector[0].select)
     .run(function(err, array) {
         console.log('x callback');
