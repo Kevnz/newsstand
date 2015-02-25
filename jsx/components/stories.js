af287e9757f533789398105bff4112506e8414a6
Kevin Isom <kevin.isom@gmail.com>
2015-02-25 23:30:34 +1300

    Get parms right

--- jsx/components/stories.js
+++ jsx/components/stories.js
@@ -15,7 +15,7 @@ var StoryList = React.createClass({
         var storyNodes = this.props.stories ? this.props.stories.map(function (item) {
             var bits = item.slug.split('/');
             return (<li key={item.slug}>
-                <Link to='story' params={{slug: bits[0], date:bits[1]}}>{item.title}</Link>
+                <Link to='story' params={{slug: bits[0], time:bits[1]}}>{item.title}</Link>
                 </li>)
         }) : <li> No Stories</li>;
         return (
